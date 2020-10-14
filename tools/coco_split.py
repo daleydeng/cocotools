cde712842d21ad94ce915dc4de6895d853716b36
daleydeng <daleydeng@qingtong123.com>
2020-10-14 18:21:47 +0800

    speedup coco_verify, remove image copy in coco_split

--- tools/coco_split.py
+++ tools/coco_split.py
@@ -7,55 +7,37 @@ import shutil
 import click
 
 @click.command()
-@click.option('--start', '-s', default=0)
-@click.option('--end', '-e', default=1000)
-@click.option('--no_img', is_flag=True)
-@click.option('--src_img_dir', default='')
-@click.option('--dst_img_dir', default='')
+@click.option('--start', '-s', type=float, default=0)
+@click.option('--end', '-e', type=float, default=0)
 @click.argument('src')
 @click.argument('dst')
-def main(start, end, no_img, src_img_dir, dst_img_dir, src, dst):
+def main(start, end, src, dst):
+    if osp.exists(dst):
+        print (f"{dst} exists!")
+        return
+
     print (f"loading {src}")
-    coco = json.load(open(src))
+    data = json.load(open(src))
     print (f"loaded {src}")
 
-    out = {i: coco[i] for i in ['info', 'licenses', 'categories']}
+    out = {i: data[i] for i in ['info', 'licenses', 'categories']}
 
-    imgs = coco['images']
-    anns = coco['annotations']
+    imgs = data['images']
+    anns = data['annotations']
 
-    if end == -1:
-        end = len(imgs)
+    if end > 0:
+        if end < 1: # ratio
+            end = end * len(imgs)
+            start = start * len(imgs)
 
-    imgs = imgs[start:end]
+        start = int(start)
+        end = int(end)
+        imgs = imgs[start:end]
+        out['images'] = imgs
+
     img_ids = set(i['id'] for i in imgs)
-
-    anns = [i for i in anns if i['image_id'] in img_ids]
-
-    out.update({
-        'images': imgs,
-        'annotations': anns,
-    })
-
-    base_d = osp.join(osp.dirname(src), '../')
-    src_name = osp.basename(src)
-
-    set_name = osp.splitext(src_name)[0].split('_')[-1]
-    if not src_img_dir:
-        src_img_dir = osp.join(base_d, set_name)
-
-    if not dst_img_dir:
-        dst_img_dir = osp.join(dst, set_name)
-
-    dst_json = osp.join(dst, 'annotations', osp.basename(src))
-    os.makedirs(osp.dirname(dst_json), exist_ok=True)
-    json.dump(out, open(dst_json, 'w'), indent=2, sort_keys=True)
-
-    if not no_img:
-        os.makedirs(dst_img_dir, exist_ok=True)
-        print ("copying image")
-        for img in tqdm(imgs):
-            shutil.copy2(osp.join(src_img_dir, img['file_name']), osp.join(dst_img_dir))
+    out['annotations'] = [i for i in anns if i['image_id'] in img_ids]
+    json.dump(out, open(dst, 'w'), indent=2, sort_keys=True)
 
 if __name__ == "__main__":
     main()
--- tools/coco_verify.py
+++ tools/coco_verify.py
@@ -6,6 +6,7 @@ import numpy.random as npr
 import multiprocessing as mp
 from functools import partial
 from pycocotools.coco import COCO
+import pycocotools.mask as coco_mask
 from skimage.io import imread, imsave
 from easydict import EasyDict as edict
 import cv2
@@ -14,7 +15,33 @@ import pylab as plt
 from tqdm import tqdm
 import click
 
-def process_one(img, img_dir, out_dir, coco, color_masks, class_dic, draw_cfg):
+def ann_to_rle(ann, img_shape):
+    """
+    Convert annotation which can be polygons, uncompressed RLE to RLE.
+    :return: binary mask (numpy 2D array)
+    """
+    h, w = img_shape
+    segm = ann['segmentation']
+    if type(segm) == list:
+        # polygon -- a single object might consist of multiple parts
+        # we merge all parts into one mask rle code
+        rles = coco_mask.frPyObjects(segm, h, w)
+        rle = coco_mask.merge(rles)
+    elif type(segm['counts']) == list:
+        # uncompressed RLE
+        rle = coco_mask.frPyObjects(segm, h, w)
+    else:
+        # rle
+        rle = ann['segmentation']
+    return rle
+
+def ann_to_mask(ann, img_shape):
+    rle = ann_to_rle(ann, img_shape)
+    m = coco_mask.decode(rle)
+    return m
+
+def process_one(d, img_dir, out_dir, color_masks, class_dic, draw_cfg):
+    img, anns = d
     fname = img['file_name']
     src_f = osp.join(img_dir, fname)
     dst_f = osp.join(out_dir, fname)
@@ -24,11 +51,8 @@ def process_one(img, img_dir, out_dir, coco, color_masks, class_dic, draw_cfg):
     I = imread(src_f)
     img0 = I.copy()
 
-    ann_ids = coco.getAnnIds(imgIds=img['id'], iscrowd=None)
-    anns = coco.loadAnns(ann_ids)
-
     for ann in anns:
-        ann_mask = coco.annToMask(ann)
+        ann_mask = ann_to_mask(ann, (img['height'], img['width']))
         ann_color_img = np.dstack([ann_mask]*3) * color_masks[ann['category_id']]
         ann_mask = ann_mask > 0.5
 
@@ -107,15 +131,15 @@ def main(no_bbox, thickness, bbox_color, text_color, font_scale, show, jobs, ann
         process_one,
         img_dir=img_dir,
         out_dir=out_dir,
-        coco=coco,
         color_masks=color_masks,
         class_dic=class_dic,
         draw_cfg=draw_cfg,
     )
 
+    works = [(i, coco.loadAnns(coco.getAnnIds(imgIds=[i['id']]))) for i in imgs]
     with mp.Pool(jobs) as p:
         with tqdm(total=len(imgs)) as pbar:
-            for i, _ in enumerate(p.imap_unordered(process_worker, imgs)):
+            for i, _ in enumerate(p.imap_unordered(process_worker, works)):
                 pbar.update()
 
 if __name__ == "__main__":
